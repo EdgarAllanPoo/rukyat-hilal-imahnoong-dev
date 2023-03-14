47244229f9f27f744149bc363b6a7adcfbc20f73
Rheza Ecaldy <rerheza@gmail.com>
2023-03-14 11:16:39 +0700

    sebuah ngetes

---
+++ components/moonPosition.tsx
@@ -0,0 +1,25 @@
+import { moonposition, sexagesimal, angle } from 'astronomia';
+
+export function moonPosition() {
+    const now = new Date();
+
+    // Calculate the moon's position
+    const moonPos = moonposition(now);
+    
+    // Get the moon's latitude and longitude
+    const { lat, lon } = moonPos;
+    
+    // Convert the latitude and longitude to sexagesimal notation
+    const latDeg = sexagesimal.strFromDD(lat);
+    const lonDeg = sexagesimal.strFromDD(lon);
+    
+    console.log(`Latitude: ${latDeg}`);
+    console.log(`Longitude: ${lonDeg}`);
+
+    return (
+        <div>
+            <h1>Moon Latitude: {latDeg}°</h1>
+            <h1>Moon Longitude: {lonDeg}°</h1>
+        </div>
+      );
+}
